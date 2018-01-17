2f7977e090f9c8fef029977ed84b7e131d19ae70
MitaliSengupta <mitali.s.auger@gmail.com>
2018-01-17 22:09:17 +0000

    Print sizes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("Size of a char: %1d byte(s)\n", sizeof(char));
+printf("Size of an int: %1d byte(s)\n", sizeof(int));
+printf("Size of a long int: %1d byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %1d byte(s)\n", sizeof(double));
+printf("Size of a float: %1d byte(s)\n", sizeof(float));
+return (0);
+}
