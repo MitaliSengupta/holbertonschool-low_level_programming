f390dfb316e7aa8dbda143fead3bf4f65ee77be6
MitaliSengupta <mitali.s.auger@gmail.com>
2018-01-17 20:21:58 +0000

    puts the thing right back

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+
 int main (void)
 {
-   puts ("\"Programming is like building a multilingual puzzle\n");
-   return 0;
+  puts("\"Programming is like building a multilingual puzzle");
+  return 0;
 }
