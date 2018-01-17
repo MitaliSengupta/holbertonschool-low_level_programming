e2b37a48bf9923d0eec4dd24b7c2e9811e4c5142
MitaliSengupta <mitali.s.auger@gmail.com>
2018-01-17 20:46:39 +0000

    Push the comment

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
-/**
-
+/*
  *main - prints strings
  *return - will pass 0 for success
-
-*/
-
-int main (void)
+ */
+int main(void)
 {
-       puts("\"Programming is like building a multilingual puzzle");
-
-       return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
