37c254efdfcad0e7755eef4bd928ea6355859a94
MitaliSengupta <mitali.s.auger@gmail.com>
2018-01-18 23:30:40 +0000

    priting hex

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	char c;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+	}
+	for (c = 'a'; c < 'g'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
