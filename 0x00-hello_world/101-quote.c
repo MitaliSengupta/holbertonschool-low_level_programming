3c2843da3458287de30ca5f18186796009852e80
MitaliSengupta <mitali.s.auger@gmail.com>
2018-01-18 00:43:21 +0000

    error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-write("and that piece of art is useful\" - Dora Korpar, 2015-10-19")
+  write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 64);
 return (1);
 }
