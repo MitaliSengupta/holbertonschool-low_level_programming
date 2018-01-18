fa95208bd09cc553716e1c5e71e2726b08321bd2
MitaliSengupta <mitali.s.auger@gmail.com>
2018-01-18 23:45:18 +0000

    print combo numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar(i + '0');
+		i++;
+		if (i == 10)
+		putchar('\n');
+		else
+			putchar(',');
+	}
+	return (0);
+}
