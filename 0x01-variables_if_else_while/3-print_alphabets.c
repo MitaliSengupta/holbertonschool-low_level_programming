b881c335a4ea1d806f558b089eecf325084014de
MitaliSengupta <mitali.s.auger@gmail.com>
2018-01-18 22:22:33 +0000

    pushing caps and non caps alphbt

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a, b;
+	for (a= 'a'; a <= 'z'; a++)
+	{
+		putchar(a);
+	}
+	for (b= 'A'; b <= 'Z'; b++)
+	{
+		putchar(b);
+	}
+	putchar('\n');
+	return (0);
+}
