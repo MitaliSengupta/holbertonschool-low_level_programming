3314565ecc0ac977d39d1d99f4029961a94de477
MitaliSengupta <mitali.s.auger@gmail.com>
2018-01-19 01:04:34 +0000

    printing combo

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, a;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (i = a + 1; i < 10; i++)
+		{
+			putchar(a + '0');
+			putchar(i + '0');
+			if ( a !=8 || i !=9)
+			{
+			putchar(',');
+			putchar(' ');
+			}
+			else
+				putchar('\n');
+		}
+	}
+	return (0);
+}
