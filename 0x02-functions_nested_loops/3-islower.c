c7cd4a31e96e19a5e6c20b7e5608612c6a98d50f
MitaliSengupta <mitali.s.auger@gmail.com>
2018-01-23 17:16:50 +0000

    to print 1 or 0 in response to lowercase or uppercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * _islower - prints 1 if lowecase else prints 0
+ * @c : takes an integer
+ * Return: Always 0.
+ */
+int _islower(int c)
+{
+	if (c <= 'z' && c >= 'a')
+		return (1);
+	else
+		return (0);
+}
