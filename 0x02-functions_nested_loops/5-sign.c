678bbccdcdb869875779f5dc6c403f4c89845ba6
MitaliSengupta <mitali.s.auger@gmail.com>
2018-01-23 17:45:50 +0000

    print sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * print_sign - prototype to print sign of number
+ * @n : integer value
+ * Return: Always 0.
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
