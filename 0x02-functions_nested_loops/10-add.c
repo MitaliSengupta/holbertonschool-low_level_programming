e8dbac29d8a54d710f6286eaba3497216d5c2ba3
MitaliSengupta <mitali.s.auger@gmail.com>
2018-01-23 22:39:00 +0000

    addition

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,13 +2,14 @@
 
 /**
  * add - adding numbers
- *
+ * @a : integer values
+ * @b : integer values
  * Return: Always 0.
  */
 int add(int a, int b)
 {
 	int c;
-	c = a + b;
 
+	c = a + b;
 	return (c);
 }
