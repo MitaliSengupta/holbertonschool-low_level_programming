800dd64cddf8ea263899d1ad776f1c7e4dcbfad8
MitaliSengupta <mitali.s.auger@gmail.com>
2018-01-23 18:07:50 +0000

    print hours and minutes

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * jack_bauer - minute, hours and everything
+ * void : is empty
+ * Return: Always 0.
+ */
+void jack_bauer(void)
+{
+	int a, b;
+
+	for (a = 0; a < 24; a++)
+	{
+		for (b = 0; b < 60; b++)
+		{
+			_putchar(a / 10 + '0');
+			_putchar(a % 10 + '0');
+			_putchar(':');
+			_putchar(b / 10 + '0');
+			_putchar(b % 10 + '0');
+			_putchar('\n');
+		}
+	}
+}
