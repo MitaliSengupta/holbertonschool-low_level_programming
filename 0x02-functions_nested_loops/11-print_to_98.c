50507bfdaa88cd2a6c77cd598741bbf229853489
MitaliSengupta <mitali.s.auger@gmail.com>
2018-01-23 23:51:21 +0000

    print number

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_to_98 - function to print numbers
+ * @n : integer value
+ * Return: Always 0.
+ */
+void print_to_98(int n)
+{
+	int a;
+
+	if (n < 98)
+	{
+		for (a = n; a < 98; a++)
+		printf("%d ", a);
+	}
+	else if (n > 98)
+		for (a = n; a > 98; a--)
+			printf("%d ", a);
+	printf("%d\n", a);
+}
