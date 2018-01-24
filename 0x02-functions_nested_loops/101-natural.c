c7d0c854cc194c0d97f370ca39a6f90dbc222e0c
MitaliSengupta <mitali.s.auger@gmail.com>
2018-01-24 02:08:22 +0000

    print numbers

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int a, b = 0;
+
+	for (a = 0; a < 1024; a++)
+	{
+		if (a % 3 == 0 || a % 5 == 0)
+		{
+			b += a;
+		}
+		printf("%d\n", a);
+	}
+	return (0);
+}
