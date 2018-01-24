255612b99988ec702f69f8be4cac41ac2aa22128
MitaliSengupta <mitali.s.auger@gmail.com>
2018-01-24 22:44:40 +0000

    fibbo

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,24 +7,38 @@
  */
 int main(void)
 {
-	unsigned long int a = 1, b = 2, c;
-	int d;
+	unsigned long int a = 0, b = 1, c;
+	unsigned long int d, e, f, g;
+	int h = 0;
 
-	printf("1, 2, ");
-
-	for (d = 3; d <= 98; d++)
+	for (h = 0; h <= 91; h++)
 	{
 		c = a + b;
-
-		printf("%lu", c);
-
-		if (d < 98)
-		{
-			printf(" , ");
-		}
 		a = b;
 		b = c;
+		printf("%lu, ", c);
 	}
+	d = a % 1000;
+	a = a / 1000;
+	e = b % 1000;
+	b = b / 1000;
+	while (h <= 98)
+	{
+		g = (d + e) / 1000;
+		f = (d + e) - g * 1000;
+		c = (a + b) + g;
+		d = e;
+		e = f;
+		a = b;
+		b = c;
+		if (f >= 100)
+			printf("%lu%lu", c, f);
+		else
+			printf("%lu0%lu", c, f);
+		if (h != 98)
+			printf(" , ");
+		h++;
+		}
 	printf("\n");
 	return (0);
 }
