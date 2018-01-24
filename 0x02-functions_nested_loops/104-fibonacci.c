efc01ff96783f7bd2c2caace910efe0bb91afb8f
MitaliSengupta <mitali.s.auger@gmail.com>
2018-01-24 07:51:43 +0000

    first 100

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	unsigned long int a = 1, b = 2, c;
+	int d;
+
+	printf("1, 2, ");
+
+	for (d = 3; d <=98; d++)
+	{
+		c = a + b;
+
+		printf("%lu", c);
+
+		if (d < 98)
+		{
+			printf(" , ");
+		}
+		a = b;
+		b = c;
+	}
+	printf("\n");
+	return (0);
+}
