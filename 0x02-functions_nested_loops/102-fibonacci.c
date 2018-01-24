eaabe3bb4ea1cacb2513de3d86e69d7dadf01409
MitaliSengupta <mitali.s.auger@gmail.com>
2018-01-24 04:23:01 +0000

    first 50 fibbo

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int count;
+	long int a = 1;
+	long int b = 2;
+	long int c = 0;
+
+	printf("%lu, ", a);
+	printf("%lu, ", b);
+	for (count = 3; count <50; count++)
+	{
+		c = a + b;
+		a = b;
+		b = c;
+
+		if( count < 49)
+		{
+			printf("%lu, ", c);
+		}
+		else
+		{
+			printf("%lu\n", c);
+		}
+	}
+	return (0);
+}
+		
