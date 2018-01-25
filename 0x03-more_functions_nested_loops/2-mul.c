3f408ce5227dd66cceaff9fd0442e8c5fe53c45d
MitaliSengupta <mitali.s.auger@gmail.com>
2018-01-25 18:14:26 +0000

    multiply 2 integers

---
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * mul - function to multiply
+ * @a : integer value
+ * @b : integer value
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
