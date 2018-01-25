726ca72fa8578495de2e0f0032c6dddda638fc7a
MitaliSengupta <mitali.s.auger@gmail.com>
2018-01-25 18:36:09 +0000

    print more numbers

---
+++ 0x03-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * more_numbers - function to print numbers
+ *
+ * Return: Always 0.
+ */
+void more_numbers(void)
+{
+	int a, b;
+
+	for (a = 0; a < 11; a++)
+	{
+		for (b = 0; b < 15; b++)
+		{
+			if (b > 9)
+			_putchar(b / 10 + '0');
+			_putchar(b % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
