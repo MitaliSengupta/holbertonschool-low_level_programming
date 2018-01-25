aa87d5c618181e7920af51032ca0d513c1391487
MitaliSengupta <mitali.s.auger@gmail.com>
2018-01-25 21:33:51 +0000

    fizzbuzz

---
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 1; a <= 100; a++)
+	{
+		if (a % 3 == 0 && a % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (a % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (a % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+			printf("%d ", a);
+	}
+	printf("\n");
+	return (0);
+}
