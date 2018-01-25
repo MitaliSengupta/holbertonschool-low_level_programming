86006c9603db245c1c35ff6443b22b345eacef99
MitaliSengupta <mitali.s.auger@gmail.com>
2018-01-25 21:38:07 +0000

    updating statement to remove space after last print

--- 0x03-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,18 +14,20 @@ int main(void)
 	{
 		if (a % 3 == 0 && a % 5 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else if (a % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else if (a % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else
-			printf("%d ", a);
+			printf("%d", a);
+		if (a != 100)
+			printf(" ");
 	}
 	printf("\n");
 	return (0);
