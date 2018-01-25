8be73041583e756af94d406bfbe8fefa025c86fc
MitaliSengupta <mitali.s.auger@gmail.com>
2018-01-25 21:52:57 +0000

    adding return

--- 0x03-more_functions_nested_loops/3-print_numbers.c
+++ 0x03-more_functions_nested_loops/3-print_numbers.c
@@ -3,15 +3,12 @@
 /**
  * print_numbers - function to print numbers
  *
- * 
  */
 void print_numbers(void)
 {
 	int a;
 
 	for (a = 0; a < 10; a++)
-	{
 		_putchar(a + '0');
-	}
 	_putchar('\n');
 }
