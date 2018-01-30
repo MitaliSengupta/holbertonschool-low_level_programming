a401e9df12c19920f10b86113dca95617353bd24
MitaliSengupta <mitali.s.auger@gmail.com>
2018-01-30 19:32:18 +0000

    Updating array to pointer style

--- 0x04-pointers_arrays_strings/6-puts2.c
+++ 0x04-pointers_arrays_strings/6-puts2.c
@@ -9,9 +9,9 @@ void puts2(char *str)
 {
 	int a;
 
-	for (a = 0; str[a] != '\0'; a++)
+	for (a = 0; *(str + a) != '\0'; a++)
 	{
-		_putchar(str[a]);
+		_putchar(*(str + a));
 		a++;
 	}
 	_putchar('\n');
