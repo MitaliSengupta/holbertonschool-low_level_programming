bf4b5cb9526cb4ee47c915c90ea54df6d04b9f76
MitaliSengupta <mitali.s.auger@gmail.com>
2018-01-30 19:30:42 +0000

    Updating betty style

--- 0x04-pointers_arrays_strings/4-print_rev.c
+++ 0x04-pointers_arrays_strings/4-print_rev.c
@@ -1,15 +1,16 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
- *
+ * print_rev - function to print reverse
+ * @s : pointer
  * Return: Always 0.
  */
 void print_rev(char *s)
 {
 	int a;
 
-	for (a = 0; s[a] != 0; a++);
+	for (a = 0; s[a] != 0; a++)
+		;
 	for (a = a - 1; a >= 0; a--)
 	{
 		_putchar(s[a]);
