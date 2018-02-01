5307e4b7ca25b19e3548f333441d9612a222eac8
MitaliSengupta <mitali.s.auger@gmail.com>
2018-02-01 18:25:23 +0000

    concatenate strings

--- 0x05-pointers_arrays_strings/0-strcat.c
+++ 0x05-pointers_arrays_strings/0-strcat.c
@@ -1,19 +1,20 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
- *
+ * _strcat - concatenate two strings
+ * @dest : pointer
+ * @src : pointer 2
  * Return: Always 0.
  */
 char *_strcat(char *dest, char *src)
 {
 	int a, b;
 
-	for(a = 0; dest[a] !='\0'; a++)
+	for (a = 0; dest[a] != '\0'; a++)
 		;
-	for(b = 0; src[b] !='\0'; b++)
+	for (b = 0; src[b] != '\0'; b++)
 	{
 		dest[a + b] = src[b];
 	}
-	return(dest);
+	return (dest);
 }
