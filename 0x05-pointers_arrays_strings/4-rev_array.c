273ff8372dbb7a29a17025a24fb3689922ccff4d
MitaliSengupta <mitali.s.auger@gmail.com>
2018-02-02 00:01:45 +0000

    reverse the array

---
+++ 0x05-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * reverse_array - reversing an array
+ * @a: an array of integers
+ * @n: the number of elements to swap
+ *
+ * Return: nothing.
+ */
+void reverse_array(int *a, int n)
+{
+	int i = 0, b, tmp;
+
+	for (b = n - 1; b > i; b--)
+	{
+		tmp = a[b];
+		a[b] = a[i];
+		a[i] = tmp;
+		i++;
+	}
+}
