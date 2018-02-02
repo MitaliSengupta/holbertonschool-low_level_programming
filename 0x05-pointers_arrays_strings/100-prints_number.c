d73b5ee5285104abe3a3734958fd1fba393a1ad0
MitaliSengupta <mitali.s.auger@gmail.com>
2018-02-02 06:24:38 +0000

    print numbers

---
+++ 0x05-pointers_arrays_strings/100-prints_number.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+
+/**
+ * print_number - function to print integers
+ * @n : integer value
+ * Return: Always 0.
+ */
+void print_number(int n)
+{
+	unsigned int a, b, count, tmp, pow;
+
+	a = n;
+	pow = b = 1;
+
+	if (n < 0)
+	{
+		a = a * -1;
+		_putchar('-');
+	}
+	tmp = a;
+
+	while (tmp > 9)
+	{
+		b++;
+		tmp = tmp / 10;
+	}
+	for (count = 1; count < b; count++)
+		pow = pow * 10;
+
+	while (pow > 1)
+	{
+		_putchar((a / pow) % 10 + '0');
+		pow = pow / 10;
+	}
+	_putchar(a % 10 + '0');
+}
