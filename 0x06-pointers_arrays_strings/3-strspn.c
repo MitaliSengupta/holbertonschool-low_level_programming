c61522dad94fa71f2ecf40c579907d5afa11ec61
mitali.s.auger@gmail.com <mitali.s.auger@gmail.com>
2018-02-05 14:40:55 -0800

    find and print the length of substring

---
+++ 0x06-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _strspn - function that gets the length of a prefix substring
+ * @s: given string
+ * @accept: find bytes
+ * Return: Always 0.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int a, b;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		for (b = 0; accept[b] != '\0'; b++)
+		{
+			if (s[a] == accept[b])
+			{
+				printf("%c\n", s[a]);
+				break;
+			}
+		}
+		if (accept[b] == '\0')
+			break;
+
+	}
+	return (a);
+}
