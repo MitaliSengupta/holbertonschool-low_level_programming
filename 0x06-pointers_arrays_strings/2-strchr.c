197607a6a8cad5c0fea7d696d0766e9d6957d33c
mitali.s.auger@gmail.com <mitali.s.auger@gmail.com>
2018-02-05 13:06:58 -0800

    find a certain character

---
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _strchr - function to locate a character
+ * @s: array strings
+ * @c: character that needs to be found
+ * Return: Always 0.
+ */
+char *_strchr(char *s, char c)
+{
+	int a;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		if (s[a] == c)
+			return (&s[a]);
+	}
+	return (&s[a]);
+}
