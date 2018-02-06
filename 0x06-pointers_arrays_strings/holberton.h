ea3c70a824e2e96885d5e557bf8076070afefc87
mitali.s.auger@gmail.com <mitali.s.auger@gmail.com>
2018-02-05 16:50:44 -0800

    added _putchar

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -1,6 +1,7 @@
 #ifndef HOLBERTON_H_
 #define HOLBERTON_H_
 
+int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
