036c86fbce3ba96059098607a7a8b305c6397110
mitali.s.auger@gmail.com <mitali.s.auger@gmail.com>
2018-02-05 20:20:18 -0800

    variable set to zero

--- 0x06-pointers_arrays_strings/8-print_diagsums.c
+++ 0x06-pointers_arrays_strings/8-print_diagsums.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- * print_diagsum - printing the sum of two diagonals
+ * print_diagsums - printing the sum of two diagonals
  * @a: array values
  * @size: size of the matrix
  * Return: Always 0.
@@ -9,15 +9,15 @@
 void print_diagsums(int *a, int size)
 {
 	int b, c;
-	int diag1, diag2;
+	int diag1 = 0, diag2 = 0;
 
 	for (b = 0; b < size * size;  b = b + (size + 1))
 	{
-		diag1 = diag1 + a[b];
+		diag1 += a[b];
 	}
 	for (c = size - 1; c < size * size - 1; c = c + (size - 1))
 	{
-		diag2 = diag2 + a[c];
+		diag2 += a[c];
 	}
 	printf("%d, ", diag1);
 	printf("%d\n", diag2);
