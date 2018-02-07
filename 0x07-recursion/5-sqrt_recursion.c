56a75aeaeaf82eda7c2c945610b902642bf114c1
mitali.s.auger@gmail.com <mitali.s.auger@gmail.com>
2018-02-07 13:04:18 -0800

    updating betty checks

--- 0x07-recursion/5-sqrt_recursion.c
+++ 0x07-recursion/5-sqrt_recursion.c
@@ -1,19 +1,25 @@
 #include "holberton.h"
 
 /**
- * _sqrt_recursion - function to print square root of given number
- * @n: number being passed
- * Return: Always 0.
+ * root_var - function to compare variables with root math
+ * @var: var being tested against n
+ * @n: n the integer value being tested for roots
+ * Return: var for root else -1
  */
 int root_var(int n, int var)
 {
 	if (var * var == n)
-		return(var);
+		return (var);
 	if (var * var < n)
-		return root_var(n, var + 1);
+		return (root_var(n, var + 1));
 	return (-1);
 }
 
+/**
+ * _sqrt_recursion - function to print square root of given number
+ * @n: number being passed
+ * Return: Always 0.
+ */
 int _sqrt_recursion(int n)
 {
 	if (n == 0)
