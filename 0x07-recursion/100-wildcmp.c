7634eb6fdd69effd3aa83a540829f5d8b19a2e61
mitali.s.auger@gmail.com <mitali.s.auger@gmail.com>
2018-02-07 21:57:03 -0800

    same string

---
+++ 0x07-recursion/100-wildcmp.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * wildcmp - function to compare two strings
+ * @s1: string one
+ * @s2: string two
+ * Return: Always 1 if same zero otherwise
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*')
+	{
+		if (s2[1] == '*')
+			return (wildcmp(s1, s2 + 1));
+		if (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1))
+			return (1);
+	}
+	return (0);
+}
