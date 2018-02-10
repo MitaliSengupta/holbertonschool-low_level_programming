351d8e3fcd21e38d8ac1e4f2a6916e642cc44788
mitali.s.auger@gmail.com <mitali.s.auger@gmail.com>
2018-02-10 15:02:59 -0800

    addition

---
+++ 0x09-argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - function to add variables
+ * @argc: number of arguments
+ * @argv: arguments being passed
+ * Return: Always 0 or 1
+ */
+int main(int argc, char **argv)
+{
+	int a, b, addition;
+
+	for (a = 1; a < argc; a++)
+	{
+		for (b = 0; argv[a][b] != '\0'; b++)
+		{
+			if (!isdigit(argv[a][b]))
+			{
+				printf("ERROR\n");
+				return (1);
+			}
+		}
+		addition = addition + atoi(argv[a]);
+	}
+	printf("%i\n", addition);
+	return (0);
+}
