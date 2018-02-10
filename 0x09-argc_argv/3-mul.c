b1a444a9c215e39894e03af85bb7253dbd99f412
mitali.s.auger@gmail.com <mitali.s.auger@gmail.com>
2018-02-10 13:50:04 -0800

    multiply 2 arguments

---
+++ 0x09-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - function to print multiplication result
+ * @argc: number of arguments
+ * @argv: arguments supplied
+ * Return: always 1 if more than or less than two arg var supplied otherwise 0.
+ */
+int main(int argc, char **argv)
+{
+	int a, b, multiplication;
+
+	if (argc <= 2)
+	{
+		printf("ERROR\n");
+		return (1);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	multiplication = a * b;
+	printf("%d\n", multiplication);
+	return (0);
+}
