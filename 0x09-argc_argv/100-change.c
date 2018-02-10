455567a7a4a2a671238e730f23b5ed8709f3f1ed
mitali.s.auger@gmail.com <mitali.s.auger@gmail.com>
2018-02-10 14:00:09 -0800

    return change calculator

---
+++ 0x09-argc_argv/100-change.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main( int argc, char **argv)
+{
+	int total, change;
+
+	if (argc < 2)
+	{
+		printf("ERROR\n");
+		return (1);
+	}
+
+	change = atoi(argv[1]);
+
+	for (total = 0; change > 0; total++)
+	{
+		if (change - 25 >= 0)
+			change = change - 25;
+		else if (change - 10 >= 0)
+			change = change - 10;
+		else if (change - 5 >= 0)
+			change = change - 5;
+		else if (change - 2 >= 0)
+			change = change - 2;
+		else if (change - 1 >= 0)
+			change = change - 1;
+	}
+	printf("%d\n", total);
+	return (0);
+}
