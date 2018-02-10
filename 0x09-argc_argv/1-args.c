884ba193f1935d65b9bdec6c333bc60a7ea3b5c4
mitali.s.auger@gmail.com <mitali.s.auger@gmail.com>
2018-02-10 13:48:41 -0800

    number of arguments

---
+++ 0x09-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ * @argc: number of arguments passed
+ * Return: Always 0
+ * @argv: arguments passed here
+ */
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
