0f35a1077f390613db69f9808cafa7d756ac7486
mitali.s.auger@gmail.com <mitali.s.auger@gmail.com>
2018-02-13 13:08:19 -0800

    string copy

---
+++ 0x0A-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - function to return ptr containing cpy of string
+ * @str: string to be copied
+ *
+ * Return: Null for failure else return ptr.
+ */
+char *_strdup(char *str)
+{
+	char *s;
+	int a, b;
+
+	if (*str == 0)
+		return (NULL);
+	for (a = 0; str[a] != '\0'; a++)
+		;
+	s = malloc(a * sizeof(*s) + 1);
+	if (s == 0)
+	{
+		return (NULL);
+	}
+	for (b = 0; b < a; b++)
+	{
+		s[b] = str[b];
+	}
+	return (s);
+}
