7de1c2b308d4a22d795190d6ae9e6f6cdeeb7b7a
mitali.s.auger@gmail.com <mitali.s.auger@gmail.com>
2018-02-13 13:34:47 -0800

    removed the brackets and dereferncing

--- 0x0A-malloc_free/1-strdup.c
+++ 0x0A-malloc_free/1-strdup.c
@@ -13,19 +13,15 @@ char *_strdup(char *str)
 	char *s;
 	int a, b;
 
-	if (*str == 0)
+	if (str == 0)
 		return (NULL);
 	for (a = 0; str[a] != '\0'; a++)
 		;
-	s = malloc(a * sizeof(*s) + 1);
+	a++;
+	s = malloc(a * sizeof(*s));
 	if (s == 0)
-	{
 		return (NULL);
-	}
 	for (b = 0; b < a; b++)
-	{
 		s[b] = str[b];
-	}
-	s[a] = '\0';
 	return (s);
 }
