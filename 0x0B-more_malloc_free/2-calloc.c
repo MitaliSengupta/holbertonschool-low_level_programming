ddc3838dab7b3caf8c0b12420efcc92736f8648a
mitali.s.auger@gmail.com <mitali.s.auger@gmail.com>
2018-02-15 14:15:23 -0800

    betty update

--- 0x0B-more_malloc_free/2-calloc.c
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 /**
- * calloc - function that allocates memory for an array, using malloc
+ * _calloc - function that allocates memory for an array, using malloc
  * @nmemb: number
  * @size: size of
  * Return: NULL if failed.
