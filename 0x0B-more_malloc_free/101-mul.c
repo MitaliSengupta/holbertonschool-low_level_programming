930e15743764bd7d6812adf88983614fbf7e5aa3
mitali.s.auger@gmail.com <mitali.s.auger@gmail.com>
2018-02-16 10:57:52 -0800

    multiply

--- 0x0B-more_malloc_free/101-mul.c
+++ 0x0B-more_malloc_free/101-mul.c
@@ -10,7 +10,7 @@
 int main(int argc, char **argv)
 {
 	char *n1, *n2, *multi_res;
-	int l1, l2, a, b, tmp = 0, carry = 0;
+	int l = 0, l1 = 0, l2 = 0, a, b, t = 0, c = 0, ten = 0;
 
 	if (argc < 3)
 	{
@@ -19,25 +19,31 @@ int main(int argc, char **argv)
 	}
 	n1 = argv[1];
 	n2 = argv[2];
+	if(!(check_number(n1) && check_number(n2)))
+	{
+		print_string("Error");
+		exit(98);
+	}
 	l1 = string_length(n1);
 	l2 = string_length(n2);
-	multi_res = _calloc(l1 + l2 + 1, sizeof(char *));
+	l = l1 + l2;
+	multi_res = _calloc(l + 1, sizeof(char *));
 	if (multi_res == 0)
 	{
 		print_string("Error");
-		exit (98);
+		exit(98);
 	}
-	for (a = 0; a < l1; a++)
+	for (a = 0; a < l1; a++, ten++)
 	{
-		for (carry = 0, b = 0; b < l2; b++)
+		for (c = 0, b = 0; b < l2; b++)
 		{
-			tmp = (n1[l1 - a - 1] - '0') * (n2[l2 -b - 1] - '0') + carry;
-			if (multi_res[(l1 + l2) - b- 1] > 0)
-				tmp += multi_res[(l1 + l2) - b - 1] - '0';
-			multi_res[(l1 + l2) - b - 1] = tmp % 10 + '0';
-			carry = tmp / 10;
+			t = (n1[l1 - a - 1] - '0') * (n2[l2 - b - 1] - '0') + c;
+			if (multi_res[l - b - ten - 1] > 0)
+				t = t + multi_res[l - b - ten - 1] - '0';
+			multi_res[l - b - ten - 1] = t % 10 + '0';
+			c = t / 10;
 		}
-		multi_res[(l1 + l2) - b - 1] += carry + '0';
+		multi_res[l - b - ten - 1] += c + '0';
 	}
 	print_string(multi_res);
 	free(multi_res);
@@ -73,8 +79,7 @@ unsigned int string_length(char *str)
 	int a;
 
 	for (a = 0; str[a] != '\0'; a++)
-		;
-	a++;
+		a++;
 	return (a);
 }
 
@@ -87,8 +92,15 @@ void print_string(char *st)
 {
 	int a;
 
+	for (a = 0; st[a] == '\0' || st[a] == '0'; a++)
+		;
+	if (st == 0)
+		_putchar('0');
 	for (a = 0; st[a] != '\0'; a++)
+	{
 		_putchar(st[a]);
+	}
+	_putchar('\n');
 }
 
 /**
@@ -100,9 +112,8 @@ void *_calloc(unsigned int number, unsigned int size)
 {
 	char *p;
 	unsigned int a;
-	void *tmp;
 
-	if (number <= 0 || size <= 0)
+	if (number == 0 || size == 0)
 		return (NULL);
 	p = malloc(number * size);
 	if (p == 0)
