20aea551aa7f73b1a808192156831b8e226849af
mitali.s.auger@gmail.com <mitali.s.auger@gmail.com>
2018-02-16 00:31:22 -0800

    updated header to remove main

--- 0x0B-more_malloc_free/holberton.h
+++ 0x0B-more_malloc_free/holberton.h
@@ -6,7 +6,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-int main(int argc, char **argv);
 int check_number(char *str);
 unsigned int string_length(char *str);
 void print_string(char *str);
