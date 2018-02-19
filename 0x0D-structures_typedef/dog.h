0227b4aedd764ea2321db69c0aa052b3e390b7fa
mitali.s.auger@gmail.com <mitali.s.auger@gmail.com>
2018-02-19 13:43:07 -0800

    header file

---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - function to typedef struct dog
+ * @name: character pointer
+ * @age: float
+ * @owner: character pointer
+ *
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
