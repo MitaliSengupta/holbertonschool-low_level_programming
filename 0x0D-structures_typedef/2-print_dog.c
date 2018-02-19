c2655a2c635f42520ffab774c763d941e6401840
mitali.s.auger@gmail.com <mitali.s.auger@gmail.com>
2018-02-19 15:11:31 -0800

    editing the age conditional

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -12,10 +12,10 @@ void print_dog(struct dog *d)
 	if (d != 0)
 	{
 		printf("Name: ");
-		(d->name == 0) ? printf("(nil)\n") : printf("%s\n", d->name);
+		d->name == NULL ? printf("(nil)\n") : printf("%s\n", d->name);
 		printf("Age: ");
-		(d->age == 0) ? printf("(nil)\n") : printf("%f\n", d->age);
+		printf("%f\n", d->age);
 		printf("Owner: ");
-		(d->owner == 0) ? printf("(nil)\n") : printf("%s\n", d->owner);
+		d->owner == NULL ? printf("(nil)\n") : printf("%s\n", d->owner);
 	}
 }
