41377a078ec2130b75f28cb15b6d8c4a998161f1
mitali.s.auger@gmail.com <mitali.s.auger@gmail.com>
2018-02-19 13:54:47 -0800

    initialize

---
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != 0)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
