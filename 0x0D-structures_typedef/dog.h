d4d325ba4b520be8019ff17e93c442415e600feb
mitali.s.auger@gmail.com <mitali.s.auger@gmail.com>
2018-02-19 13:44:19 -0800

    header file update

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -5,7 +5,6 @@
  * @name: character pointer
  * @age: float
  * @owner: character pointer
- *
  */
 typedef struct dog
 {
@@ -13,7 +12,3 @@ typedef struct dog
 	float age;
 	char *owner;
 } dog_t;
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
