72bd950f1db1a723143e594b3e718a74ef9779e2
mitali.s.auger@gmail.com <mitali.s.auger@gmail.com>
2018-02-19 16:56:41 -0800

    update

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -20,22 +20,19 @@ dog_t *new_dog(char *name, float age, char *owner)
 		;
 	doggo = malloc(sizeof(dog_t));
 	if (doggo == 0)
-       		return (NULL);
+		return (NULL);
 	doggo->name = malloc(a * sizeof(doggo->name));
 	if (doggo->name == 0)
-        	return (NULL);
+		return (NULL);
 	for (c = 0; c < a; c++)
-	{
 		doggo->name[c] = name[c];
-	}
 	doggo->name[c] = '\0';
 	doggo->age = age;
 	doggo->owner = malloc(b * sizeof(doggo->owner));
 	if (doggo->owner == 0)
 		return (NULL);
 	for (c = 0; c < b; c++)
-	{
 		doggo->owner[c] = owner[c];
-	}
+	doggo->owner[c] = '\0';
 	return (doggo);
 }
