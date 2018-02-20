7baa7cd076d32c043a75f50d142fc0bcc62f8d9d
mitali.s.auger@gmail.com <mitali.s.auger@gmail.com>
2018-02-19 17:13:57 -0800

    removing null

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -32,7 +32,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 	}
 	for (c = 0; c < a; c++)
 		doggo->name[c] = name[c];
-	doggo->name[c] = '\0';
 	doggo->age = age;
 	doggo->owner = malloc(b * sizeof(doggo->owner));
 	if (doggo->owner == 0)
@@ -42,6 +41,5 @@ dog_t *new_dog(char *name, float age, char *owner)
 	}
 	for (c = 0; c < b; c++)
 		doggo->owner[c] = owner[c];
-	doggo->owner[c] = '\0';
 	return (doggo);
 }
