f65aacb5c417700ed500a8a1e2d80bb59990b39f
mitali.s.auger@gmail.com <mitali.s.auger@gmail.com>
2018-02-19 16:51:42 -0800

    update logic

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -11,33 +11,30 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	int a, b, c;
+	int a = 0, b = 0, c = 0;
 	dog_t *doggo;
 
-	for (a = 0; name[a] != '\0'; a++)
-		;
-	for (b = 0; owner[b] != '\0'; b++)
-		;
+	while (name[a])
+		a++;
+	while (owner[b])
+		b++;
 	doggo = malloc(sizeof(dog_t));
-
 	if (doggo == 0)
-		return (NULL);
+       		return (NULL);
 	doggo->name = malloc(a * sizeof(doggo->name));
-	if (doggo == 0)
-		return (NULL);
+	if (doggo->name == 0)
+        	return (NULL);
 	for (c = 0; c < a; c++)
 	{
 		doggo->name[c] = name[c];
 	}
-	doggo->name[c] = '\0';
 	doggo->age = age;
 	doggo->owner = malloc(b * sizeof(doggo->owner));
-	if (doggo == 0)
+	if (doggo->owner == 0)
 		return (NULL);
 	for (c = 0; c < b; c++)
 	{
 		doggo->owner[c] = owner[c];
 	}
-	doggo->owner[c] = '\0';
 	return (doggo);
 }
