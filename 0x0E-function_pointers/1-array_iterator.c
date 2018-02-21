bd9659b1d954ab0c6536cae403bad0ba835351a7
mitali.s.auger@gmail.com <mitali.s.auger@gmail.com>
2018-02-21 12:07:11 -0800

    updating if statement

--- 0x0E-function_pointers/1-array_iterator.c
+++ 0x0E-function_pointers/1-array_iterator.c
@@ -11,7 +11,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t a;
 
-	if (array != 0 || action != 0)
+	if (array != 0 && action != 0)
 	{
 		for (a = 0; a < size; a++)
 			action(array[a]);
