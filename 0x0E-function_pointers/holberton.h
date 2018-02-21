6af4bdf26a2b9aee5a7ff2b1f605e41447b91ed6
mitali.s.auger@gmail.com <mitali.s.auger@gmail.com>
2018-02-21 11:03:33 -0800

    Initial Commit

---
+++ 0x0E-function_pointers/holberton.h
@@ -0,0 +1,6 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
