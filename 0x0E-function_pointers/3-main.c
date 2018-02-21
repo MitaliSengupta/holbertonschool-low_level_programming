0d087742e73e9d34dce763f51a88b1607023c835
mitali.s.auger@gmail.com <mitali.s.auger@gmail.com>
2018-02-21 14:40:00 -0800

    updating the if statement

--- 0x0E-function_pointers/3-main.c
+++ 0x0E-function_pointers/3-main.c
@@ -18,6 +18,11 @@ int main(int argc, char **argv)
 		printf("Error\n");
 		exit(98);
 	}
+	if (argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
 	res = get_op_func(argv[2]);
 	if (res == 0)
 	{
