3e71eaf157cfa50f759156832fb9940331e6af03
mitali.s.auger@gmail.com <mitali.s.auger@gmail.com>
2018-02-21 12:11:14 -0800

    fixing betty and statements

--- 0x0E-function_pointers/2-int_index.c
+++ 0x0E-function_pointers/2-int_index.c
@@ -2,20 +2,22 @@
 #include <stdlib.h>
 
 /**
- *
- *
- *
- *
+ * int_index -function to search for integers
+ * @size: size of array
+ * @array: array of integers
+ * @cmp: pointer to function that compares value
+ * Return: -1 for failure else return index
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int a;
 
-	if (array != 0 || size > 0 || cmp != 0)
+	if (array != 0 && size > 0 && cmp != 0)
 	{
 		for (a = 0; a < size; a++)
 			if (cmp(array[a]))
 				return (a);
 	}
-	return (-1);
+	else
+		return (-1);
 }
