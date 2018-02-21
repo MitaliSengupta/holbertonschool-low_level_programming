0b67f65ee7499af4c6e052afc98b0dc16dd0a3af
mitali.s.auger@gmail.com <mitali.s.auger@gmail.com>
2018-02-21 11:11:53 -0800

    printing name

---
+++ 0x0E-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * print_name - function that prints a name
+ * @name: string pointer
+ * @f: pointer
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != 0 || f != 0)
+		f(name);
+}
--- 0x0E-function_pointers/function_pointers.h
+++ 0x0E-function_pointers/function_pointers.h
@@ -1,6 +1,6 @@
-#ifndef FUNCTION_POINTERS.H
-#define FUNCTION_POINTERS.H
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
 
 void print_name(char *name, void (*f)(char *));
-void array_iterator(int *array, size_t size, void (*action)(int));
-int int_index(int *array, int size, int (*cmp)(int));
+
+#endif
