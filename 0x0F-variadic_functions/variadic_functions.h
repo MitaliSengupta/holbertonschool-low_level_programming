bdc080730946be5f7ef626c4f80515b78bb03e3e
mitali.s.auger@gmail.com <mitali.s.auger@gmail.com>
2018-02-23 02:21:52 -0800

    adding endif

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -5,3 +5,5 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 int _putchar(char c);
+
+#endif
