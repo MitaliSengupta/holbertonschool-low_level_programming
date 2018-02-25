864b8689cfcc039f06dc993110b08202c3e8d41a
mitali.s.auger@gmail.com <mitali.s.auger@gmail.com>
2018-02-24 18:42:36 -0800

    update

--- 0x0F-variadic_functions/2-print_strings.c
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -21,7 +21,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		if (str == NULL)
 			printf("(nil)");
 		printf("%s", str);
-		if (a != (n - 1) && separator != NULL)
+		if ((a != n - 1) && separator != NULL)
 			printf("%s", separator);
 	}
 	printf("\n");
