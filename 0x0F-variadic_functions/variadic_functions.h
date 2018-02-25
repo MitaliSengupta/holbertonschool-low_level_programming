9dbcfcb247f5674855b4511d41854d42cdd57cfe
mitali.s.auger@gmail.com <mitali.s.auger@gmail.com>
2018-02-24 18:50:39 -0800

    update header

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -2,6 +2,17 @@
 #define VARIADIC_FUNCTIONS_H
 #include <stdarg.h>
 
+/**
+ * struct what_format - defines representing a data type
+ * @type: the data type
+ * @f: function that prints data type in correct fmt
+ */
+typedef struct what_format
+{
+	char *type;
+	void (*f)();
+} what_format;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
@@ -11,16 +22,4 @@ void _printchar(va_list list);
 void _printstr(va_list list);
 void _printfloat(va_list list);
 void _printint(va_list list);
-
-/**
- * struct what_format - defines representing a data type
- * @type: the data type
- * @f: function that prints data type in correct fmt
- *
- */
-typedef struct what_format
-{
-	char *type;
-	void (*f)();
-}what_format;
 #endif
