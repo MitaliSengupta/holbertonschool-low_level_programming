56bc0925e544f01edfd6a6cac48b425caff3170c
mitali.s.auger@gmail.com <mitali.s.auger@gmail.com>
2018-03-01 12:58:29 -0800

    adding comments

--- 0x11-singly_linked_lists/101-hello_holberton.asm
+++ 0x11-singly_linked_lists/101-hello_holberton.asm
@@ -1,16 +1,17 @@
-	extern printf
-	section .data
-	msg db 'Hello, Holberton', 0
-fmt:	db "%s", 10, 0
+;; declare required c functions
+	extern printf		; the c function to be called
+	section .data		; data section initializing variables
+msg:	db 'Hello, Holberton', 0 ; string 0
+fmt:	db "%s", 10, 0		 ; printf format for new line and 0
 
-	section .text
-	global main
-main:
-	push rbp
-	mov rdi, fmt
-	mov rsi, msg
-	mov rax, 0
-	call printf
-	pop rbp
-	mov rax, 0
-	int 0x80
+	section .text		; code
+	global main		; gcc entry point
+main:				; program entry point
+	push rbp		;creating stack frame
+	mov rdi,fmt
+	mov rsi,msg
+	mov rax,0
+	call printf		; calling required function
+	pop rbp			; clean stack
+	mov rax,0		; error value
+	ret			; return
