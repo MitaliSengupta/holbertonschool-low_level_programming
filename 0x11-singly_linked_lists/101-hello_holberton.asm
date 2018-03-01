7b4bde0fd4c59dec7114f47628cc236997695147
mitali.s.auger@gmail.com <mitali.s.auger@gmail.com>
2018-03-01 12:48:44 -0800

    same as before

---
+++ 0x11-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,15 @@
+	section .text
+	global main
+main:
+	mov ebx, 1
+	mov ecx, msg
+	mov edx, len
+	mov eax, 4
+	int 0x80
+
+	mov eax, 0
+	int 0x80
+
+	section .data
+	msg db 'Hello, Holberton', 0xa
+	len equ $ - msg
