f82c7c0d75111357314b1e3f4f1f3c64070e0923
mitali.s.auger@gmail.com <mitali.s.auger@gmail.com>
2018-03-01 13:06:46 -0800

    priority print to stdout

---
+++ 0x11-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ *
+ *
+ */
+void __attribute__((constructor)) priority()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
