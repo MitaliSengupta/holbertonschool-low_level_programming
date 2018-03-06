cdd7d17230524f5355397841c4c80e276b448ebf
mitali.s.auger@gmail.com <mitali.s.auger@gmail.com>
2018-03-06 13:08:17 -0800

    free list

---
+++ 0x12-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+/**
+ * free_listint_safe - function to free list
+ * @h: pointer to the pointer of the list
+ * Return: count
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	size_t count_new = 0, count_comp = 0;
+	listint_t *tmp, *head, *comp;
+
+	if (h == NULL || *h == NULL)
+		return (0);
+	head = comp = tmp = *h;
+
+	while (head != NULL)
+	{
+		comp = *h;
+		while (count_new > count_comp)
+		{
+			if (tmp == comp)
+			{
+				*h = NULL;
+				return (count_new);
+			}
+			count_comp++;
+			comp = comp->next;
+		}
+		count_comp = 0;
+		count_new++;
+		tmp = head->next;
+		free(head);
+		head = tmp;
+	}
+	count_new = 0;
+	*h = NULL;
+	return (count_new);
+}
