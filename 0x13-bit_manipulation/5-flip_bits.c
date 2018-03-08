74fc7a7b150bb6b6a48d01a5b1a5ef588838c474
mitali.s.auger@gmail.com <mitali.s.auger@gmail.com>
2018-03-08 15:40:25 -0800

    change bits

--- 0x13-bit_manipulation/5-flip_bits.c
+++ 0x13-bit_manipulation/5-flip_bits.c
@@ -1,10 +1,10 @@
 #include "holberton.h"
 
 /**
- *
- *
- *
- *
+ * flip_bits - function to count number of bits and flip
+ * @n: number to be flipped
+ * @m: number to be flipped to
+ * Return: number of bits required
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
