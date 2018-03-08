a01e607b849a509149d01e1e2d0c623052ccdff2
mitali.s.auger@gmail.com <mitali.s.auger@gmail.com>
2018-03-08 13:46:50 -0800

    update betty

--- 0x13-bit_manipulation/2-get_bit.c
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -1,9 +1,10 @@
 #include "holberton.h"
 
 /**
- *
- *
- *
+ * get_bit - function to get the bit at the given index
+ * @n: number to check the bit in
+ * @index: index to check the bit at
+ * Return: the bit at the index else -1
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
