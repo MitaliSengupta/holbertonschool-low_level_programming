69fbb9d74d18f2de9d014f898fc9ca187dc0fc91
mitali.s.auger@gmail.com <mitali.s.auger@gmail.com>
2018-03-08 18:19:54 -0800

    betty

--- 0x13-bit_manipulation/100-get_endianness.c
+++ 0x13-bit_manipulation/100-get_endianness.c
@@ -4,11 +4,11 @@
  * get_endianness - function that checks endianness
  * @void: void
  * Return: 0 or 1
- *
  */
 int get_endianness(void)
 {
 	unsigned int x = 1;
 	char *c = (char *) &x;
-	return (int) *c;
+
+	return ((int) *c);
 }
