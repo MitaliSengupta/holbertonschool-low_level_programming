c6f21e55a5917e0817736ab3d47c4d2d9515312c
mitali.s.auger@gmail.com <mitali.s.auger@gmail.com>
2018-03-13 15:43:46 -0700

    buffer return

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -22,7 +22,7 @@ int main(int argc, char **argv)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	buff = malloc(BUFFSIZE * sizeof(char));
 	if (buff == NULL)
-		return (-1);
+		return (1);
 	input_r = read(input, buff, BUFFSIZE);
 	if (input_r == -1)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
