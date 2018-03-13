2b8639000c5ae14d464189d4fbace5d7fdbc6637
mitali.s.auger@gmail.com <mitali.s.auger@gmail.com>
2018-03-13 13:12:04 -0700

    create file

---
+++ 0x14-file_io/1-create_file.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * create_file - function to create file
+ * @filename: pointer to the file name to be created
+ * @text_content: str to be written in the file
+ * Return: 1 success -1 fail
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int input, output, count = 0;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content == NULL)
+		return (-1);
+
+	input = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+
+	if (input == -1)
+		return (-1);
+	for (; text_content[count]; count++)
+		;
+	output = write(input, text_content, count);
+	if (output == -1)
+		return (-1);
+	close(input);
+	return (1);
+}
