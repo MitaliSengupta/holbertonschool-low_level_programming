cb16e0052308e1feb2a9b26262415e85a3504c3a
mitali.s.auger@gmail.com <mitali.s.auger@gmail.com>
2018-03-13 14:18:45 -0700

    testing for malloc

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -14,7 +14,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return (0);
 
-	buff = malloc(sizeof(char *) * letters);
+	buff = malloc(sizeof(char) * letters);
 	if (buff == NULL)
 		return (0);
 
