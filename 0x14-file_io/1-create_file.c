ce6456463aeb0fa9df987a16d002ca801b41c9d5
mitali.s.auger@gmail.com <mitali.s.auger@gmail.com>
2018-03-13 13:22:39 -0700

    text content check moved

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -12,13 +12,12 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	if (text_content == NULL)
-		return (1);
-
 	input = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 
 	if (input == -1)
 		return (-1);
+	if (text_content == NULL)
+		return (1);
 
 	for (; text_content[count] != '\0'; count++)
 		;
