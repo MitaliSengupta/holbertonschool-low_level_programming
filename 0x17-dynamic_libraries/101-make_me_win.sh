b6f84e6929ee25ec013cf58dfcdac6172bde59a7
mitali.s.auger@gmail.com <mitali.s.auger@gmail.com>
2018-04-18 21:14:40 -0700

    changed path

--- 0x17-dynamic_libraries/101-make_me_win.sh
+++ 0x17-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget https://github.com/MitaliSengupta/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/libtest.so  -P /etc
-export LD_PRELOAD=/etc/libtest.so
+wget https://github.com/MitaliSengupta/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/libtest.so  -P /usr/bin
+export LD_PRELOAD=/usr/bin/libtest.so
