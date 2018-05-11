9ecbdb594dad2fb372ad8cb8877036bd3eb30821
MitaliSengupta <mitali.s.auger@gmail.com>
2018-05-11 13:36:33 -0700

    betty

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -1,5 +1,4 @@
 #include "hash_tables.h"
-
 /**
  * hash_table_create - creating a new hash table
  * @size: size of the hash table
@@ -11,16 +10,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	if (size == 0)
 		return (NULL);
-
 	new = malloc(sizeof(hash_table_t));
-
 	if (new == NULL)
 		return (NULL);
-
 	new->size = size;
 	new->array = malloc(sizeof(hash_node_t *) * size);
-
 	if (new->array == NULL)
-		return(NULL);
+		return (NULL);
 	return (new);
 }
