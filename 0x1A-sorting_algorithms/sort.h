a6d90e8c9d25961c68d97dc674433a5622a02a7e
MitaliSengupta <mitali.s.auger@gmail.com>
2018-05-29 12:47:56 -0700

    quick sort update

--- 0x1A-sorting_algorithms/sort.h
+++ 0x1A-sorting_algorithms/sort.h
@@ -23,6 +23,7 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+size_t partition(int *array, size_t start, size_t pivot, size_t size);
+void sort(int *array, size_t start, size_t end, size_t size);
 void swap(int *a, int *b);
-
 #endif
