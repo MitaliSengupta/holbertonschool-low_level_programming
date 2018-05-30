f735dc8f28292c3be1f2ac3601b2cf538be33bb0
MitaliSengupta <mitali.s.auger@gmail.com>
2018-05-30 12:18:49 -0700

    counting sort

---
+++ 0x1A-sorting_algorithms/102-counting_sort.c
@@ -0,0 +1,55 @@
+#include "sort.h"
+
+/**
+ *
+ *
+ *
+ *
+ */
+void counting_sort(int *array, size_t size)
+{
+	int n = 0, *count = NULL, *new = NULL, max = 0;
+	size_t i = 0, tmp = 0, total = 0;
+
+	if (array == NULL || size <= 1)
+		return;
+
+	new = malloc(sizeof(*array) * size);
+
+	if (new == NULL)
+		return;
+
+	for (; i < size; i++)
+	{
+		new[i] = array[i];
+		if (array[i] > max)
+			max = array[i] + 1;
+	}
+	count = malloc(sizeof(*count) * max);
+	if (count == NULL)
+	{
+		free(new);
+		return;
+	}
+
+	for (i = 0; i < size; i++)
+		count[array[i]]++;
+	total = 0;
+
+	for (n = 0; n < max; n++)
+	{
+		tmp = count[n];
+		count[n] += total;
+		total += tmp;
+	}
+
+	print_array(count, max);
+
+	for (i = 0; i < size; i++)
+	{
+		array[count[new[i]] - 1] = new[i];
+		count[new[i]]--;
+	}
+	free(count);
+	free(new);
+}
