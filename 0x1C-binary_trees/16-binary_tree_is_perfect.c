1e56c104def8bf81c93d2f5532b808236f198037
MitaliSengupta <mitali.s.auger@gmail.com>
2018-07-05 16:33:17 -0700

    check if tree is perfect

---
+++ 0x1C-binary_trees/16-binary_tree_is_perfect.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+#include "14-binary_tree_balance.c"
+/**
+ * binary_tree_is_perfect - function to check if a binary tree is perfect
+ * @tree: pointer to the rood node of tree to check
+ * Return: Null = 0
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	int left, right;
+
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL || tree->right == NULL)
+		return (1);
+	if (binary_tree_balance(tree) != 0)
+		return (0);
+	if (tree->left && tree->right)
+	{
+		left = binary_tree_is_perfect(tree->left);
+		right = binary_tree_is_perfect(tree->right);
+		return (left && right);
+	}
+	return (0);
+}
