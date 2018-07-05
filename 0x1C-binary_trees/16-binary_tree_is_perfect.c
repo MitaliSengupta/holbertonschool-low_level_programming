4e188b9159afa7b38d2c61cf835334b67a563f90
MitaliSengupta <mitali.s.auger@gmail.com>
2018-07-05 16:37:22 -0700

    check if tree is perfect

--- 0x1C-binary_trees/16-binary_tree_is_perfect.c
+++ 0x1C-binary_trees/16-binary_tree_is_perfect.c
@@ -12,7 +12,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 	if (tree->left == NULL || tree->right == NULL)
-		return (1);
+		return (0);
 	if (binary_tree_balance(tree) != 0)
 		return (0);
 	if (tree->left && tree->right)
