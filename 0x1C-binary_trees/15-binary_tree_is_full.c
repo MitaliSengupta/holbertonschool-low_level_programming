803d94442a7776034afc38e0dd336913467e35a2
MitaliSengupta <mitali.s.auger@gmail.com>
2018-07-05 16:12:09 -0700

    adding betty check

--- 0x1C-binary_trees/15-binary_tree_is_full.c
+++ 0x1C-binary_trees/15-binary_tree_is_full.c
@@ -1,9 +1,9 @@
 #include "binary_trees.h"
+
 /**
- *
- *
- *
- *
+ * binary_tree_is_full - function to check if binary tree is full
+ * @tree: tree
+ * Return: return value
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
@@ -16,7 +16,8 @@ int binary_tree_is_full(const binary_tree_t *tree)
 		if (binary_tree_is_full(tree->left) != 1)
 			return (0);
 	}
-	if ((tree->left && tree->right == NULL) || (tree->left == NULL && tree->right))
+	if ((tree->left && tree->right == NULL) ||
+	    (tree->left == NULL && tree->right))
 		return (0);
 	return (1);
 }
