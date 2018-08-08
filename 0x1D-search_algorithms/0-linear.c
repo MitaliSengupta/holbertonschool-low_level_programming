ea17ddc31fc23648fcc9395d6fd18cbc5ce5d4ae
MitaliSengupta <mitali.s.auger@gmail.com>
2018-08-08 16:22:08 +0000

    linear algorithm

---
+++ 0x1D-search_algorithms/0-linear.c
@@ -0,0 +1,24 @@
+#include "search_algos.h"
+
+/**
+ * linear_search: function that uses linear search algorithm
+ * @array: array to be searched
+ * @size: size of array
+ * @value: value to be searched
+ * Return: -1 if not found else return the first index
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (array == NULL)
+		return(-1);
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
+}
---
+++ 0x1D-search_algorithms/search_algos.h
@@ -0,0 +1,9 @@
+#ifndef _SEARCH_ALGOS_H_
+#define _SEARCH_ALGOS_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int linear_search(int *array, size_t size, int value);
+
+#endif
