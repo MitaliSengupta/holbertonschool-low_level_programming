f461cd67a64eda44912fd6d3da7accaf1f0c5bda
MitaliSengupta <mitali.s.auger@gmail.com>
2018-08-08 17:01:41 +0000

    adding updated header

--- 0x1D-search_algorithms/search_algos.h
+++ 0x1D-search_algorithms/search_algos.h
@@ -4,6 +4,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int binary_search(int *array, size_t size, int value);
+int _search(int *array, size_t beg, size_t size, int value);
 int linear_search(int *array, size_t size, int value);
 
 #endif
