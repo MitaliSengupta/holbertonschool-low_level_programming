604391713e645e4f827fb68b0cae0c18e95b6316
MitaliSengupta <mitali.s.auger@gmail.com>
2018-08-08 16:24:54 +0000

    fixing betty

--- 0x1D-search_algorithms/0-linear.c
+++ 0x1D-search_algorithms/0-linear.c
@@ -1,7 +1,7 @@
 #include "search_algos.h"
 
 /**
- * linear_search: function that uses linear search algorithm
+ * linear_search - function that uses linear search algorithm
  * @array: array to be searched
  * @size: size of array
  * @value: value to be searched
@@ -12,7 +12,7 @@ int linear_search(int *array, size_t size, int value)
 	size_t i;
 
 	if (array == NULL)
-		return(-1);
+		return (-1);
 
 	for (i = 0; i < size; i++)
 	{
